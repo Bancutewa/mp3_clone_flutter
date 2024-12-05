12601a4cbe7e145dadc8fa2831327bebb0eda48f
bancutewa10304 <tranngoctien10304@gmail.com>
2024-12-05 14:06:29 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Zing MP333',
+      title: 'Zing MP3',
       theme: ThemeData(
         // This is the theme of your application.
         //
